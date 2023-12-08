5afdfcf38eddd187e8ad3059e511fa9312cf03e6
Frank <f.vanleeuwen@unovica.nl>
2023-12-08 07:58:23 +0100

    convert(only)

--- programs/convert.c
+++ programs/convert.c
@@ -10,9 +10,9 @@
 /*  along with this program.  If not, see <http://www.gnu.org/licenses/>.    */
 /*****************************************************************************/
 
-/* convert.c: convert any to any format (multiple output support).
- * Initially intended for WASM/js; a main() could be created later,
- * possibly consolidating existing programs.
+/* convert.c: simple API to read/write any format.
+ * Single read, multiple write supported.
+ * You should call done() once you're done.
  * 
  * written by Frank van Leeuwen
  */
@@ -23,56 +23,112 @@
 #  include "in_json.h"
 #  include "out_json.h"
 #endif
-#include "in_dxf.h"
-#include "out_dxf.h"
+#ifndef DISABLE_DXF
+#  include "in_dxf.h"
+#  include "out_dxf.h"
+#endif
 
-// Note: inputs are sanitized in js
-void convert(
-    unsigned char* in, size_t size, const char* version, const char* type,
-    unsigned char* dxf, size_t dxf_size, const char* dxf_version,
-    unsigned char* dxfb, size_t dxfb_size, const char* dxfb_version,
-    unsigned char* json, size_t json_size) {
+Dwg_Data dwg;
+int readError = DWG_ERR_IOERROR;
 
-  Dwg_Data dwg;
-  Bit_Chain dat = { 0 };
-  int error = 0;
+EXPORT
+void done() {
+  if (!readError)
+    dwg_free(&dwg);
+}
 
+EXPORT
+void read(unsigned char* buf, size_t size, const char* type, unsigned char level) {
+  Bit_Chain dat = EMPTY_CHAIN (0);
+  dat.chain = buf;
+  dat.size = size;
+  dat.opts = level & DWG_OPTS_LOGLEVEL;
   memset (&dwg, 0, sizeof (Dwg_Data));
-  //dwg.opts = opts;
-  if (version) // hint the importer
-    dwg.header.version = dat.version = version;
 
-  dat.chain = in;
+  if (!readError)
+    done();
+
+  int error = 0;
   if (!strcmp(type, "dwg")) {
     error = dwg_decode (&dat, &dwg);
+    if (error)
+      dwg_free(&dwg);
   }
+#ifndef DISABLE_DXF
   else if (!strcmp(type, "dxf")) {
     error = dwg_read_dxf (&dat, &dwg);
   }
   else if (!strcmp(type, "dxfb")) {
     error = dwg_read_dxfb (&dat, &dwg);
   }
+#endif
+#ifndef DISABLE_JSON
   else if (!strcmp(type, "json")) {
     error = dwg_read_json (&dat, &dwg);
   }
-  if (error)
-    return error;
+#endif
+  else if (!strcmp(type, "geojson")) {
+    error = DWG_ERR_NOTYETSUPPORTED;
+  }
+  else if (!strcmp(type, "yaml")) {
+    error = DWG_ERR_NOTYETSUPPORTED;
+  }
+  else if (!strcmp(type, "xml")) {
+    error = DWG_ERR_NOTYETSUPPORTED;
+  }
+  else {
+    error = DWG_ERR_INVALIDTYPE;
+  }
+  readError = error;
+  return error;
+}
 
-  if (dxf && dxf_size) {
+EXPORT
+void write(unsigned char* buf, size_t size, const char* type, const char* version, int level, bool minimal) {
+  Bit_Chain dat = EMPTY_CHAIN (0);
+  dat.chain = buf;
+  dat.size = size;
+  dat.opts = (level & DWG_OPTS_LOGLEVEL) | (minimal ? DWG_OPTS_MINIMAL : 0);
+
+  if (readError)
+    return DWG_ERR_IOERROR;
+  if (version && *version)
+    dwg.header.version = dwg_version_as(version);
+    if (dwg.header.version == R_INVALID) {
+      return DWG_ERR_INVALIDTYPE;
+    }
+
+  int error = 0;
+  if (!type) {
+    error = DWG_ERR_INVALIDTYPE;
+  }
+  else if (!strcmp(type, "dwg")) {
+    error = dwg_encode(&dat, &dwg);
+  }
+#ifndef DISABLE_DXF
+  else if (!strcmp(type, "dxf")) {
     error = dwg_write_dxf(&dat, &dwg);
-    if (error)
-      return error;
   }
-  if (dxfb && dxfb_size) {
+  else if (!strcmp(type, "dxfb")) {
     error = dwg_write_dxfb(&dat, &dwg);
-    if (error)
-      return error;
   }
-  if (json && json_size) {
+#endif
+#ifndef DISABLE_JSON
+  else if (!strcmp(type, "json")) {
     error = dwg_write_json(&dat, &dwg);
-    if (error)
-      return error;
   }
-
+  else if (!strcmp(type, "geojson")) {
+    error = dwg_write_geojson(&dat, &dwg);
+  }
+#endif
+  else if (!strcmp(type, "yaml")) {
+    error = DWG_ERR_NOTYETSUPPORTED;
+  }
+  else if (!strcmp(type, "xml")) {
+    error = DWG_ERR_NOTYETSUPPORTED;
+  }
+  else {
+    error = DWG_ERR_INVALIDTYPE;
+  }
   return error;
 }
